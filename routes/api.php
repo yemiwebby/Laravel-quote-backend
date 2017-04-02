24d1b031f45257b7e6b4d2e80c5f3261e2211159
Oluyemi <kayoluyem23@gmail.com>
2017-04-02 07:15:06 +0100

    Added the API endpoint and also created the migration file

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,33 @@
 <?php
 
-use Illuminate\Http\Request;
+    use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | is assigned the "api" middleware group. Enjoy building your API!
+    |
+    */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::post('/quote',[
+        'uses'       =>      'QuoteController@postQuote'
+    ]);
+
+
+    Route::get('/quotes',[
+        'uses'       =>      'QuoteController@getQuote'
+    ]);
+
+    Route::put('/quote/{id}',[
+        'uses'       =>      'QuoteController@putQuote'
+    ]);
+
+    Route::delete('/quote/{id}',[
+        'uses'       =>      'QuoteController@deleteQuote'
+    ]);
+
+
